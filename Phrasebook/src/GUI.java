0f04f7f5f26d07741d2e969b9b3947ee355b9359
nogitsune-youkai <nikita.elin2011@yandex.ru>
2023-03-20 02:57:57 +0300

    implemented ability to add words to Phrasebook

--- Phrasebook/src/GUI.java
+++ Phrasebook/src/GUI.java
@@ -1,31 +1,39 @@
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.scene.control.TextInputDialog;
 
 public class GUI {
-	private Button addNewWord = new Button("Добавить слово");
-	private Button saveButton = new Button("Сохранить");
-	private TextField textInput = new TextField();
-	private VBox vBoxButtons = new VBox();
-	
+	 private Button addNewWord = new Button("Добавить слово");
+	 private Button saveButton = new Button("Сохранить");
+	 private TextArea textInput = new TextArea();
+	 private VBox vBoxButtons = new VBox(); // VBox for buttons
+	 private HBox hBoxText = new HBox();
+	 
+	 
 	 public void start(Stage primaryStage) {
-		 
+		
+		
 	    addNewWord.setOnAction(event -> { addWord();});
 	    
 		StackPane root = new StackPane(); //the root node's size tracks the scene's size and 
         //changes when the stage is resized by a user
+		hBoxText.getChildren().add(textInput);
 		vBoxButtons.getChildren().add(addNewWord);
 		vBoxButtons.getChildren().add(saveButton);
-		vBoxButtons.setAlignment(Pos.BASELINE_RIGHT);
-        vBoxButtons.setSpacing(3);
-		//root.setAlignment(Pos.BASELINE_RIGHT);
-        root.getChildren().add(vBoxButtons);
-        root.getChildren().add(textInput);
+		textInput.setMinWidth(694);
+		textInput.setMinHeight(597);
+		hBoxText.getChildren().add(vBoxButtons);
+		hBoxText.setAlignment(Pos.BASELINE_RIGHT);
+		textInput.setEditable(false);
+        
+
+        root.getChildren().add(hBoxText);
         // Setting up a scene, JavaFX Scene class is the container for all content 
         // which makes the scene visible in a given pixel size. 
         Scene scene = new Scene(root, 800, 600);
@@ -41,6 +49,6 @@ public class GUI {
 		inputDialog.setTitle("Добавить новое слово");
 		inputDialog.setContentText("Введите слово или словосочетание");		
 		inputDialog.showAndWait();
-		textInput.setText(inputDialog.getResult());
+		textInput.appendText(inputDialog.getResult() + "\n");
 	}
 }
